9a8beb1f34c6bf08ac0982888fe179b530a41b51
haupc <haupc.uet@gmail.com>
2021-06-02 15:39:22 +0700

    fix

--- car/rpc/car.findTrip_handler.go
+++ car/rpc/car.findTrip_handler.go
@@ -3,7 +3,6 @@ package car
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"log"
 
 	"github.com/haupc/cartransplant/grpcproto"
@@ -14,9 +13,6 @@ func (c *carServer) FindTrip(ctx context.Context, req *grpcproto.FindTripRequest
 	if err != nil {
 		log.Printf("FindTrip - Error: %v", err)
 		return nil, err
-	}
-	if routes == nil || len(routes) == 0 {
-		return nil, errors.New("No trip found")
 	}
 	byteData, err := json.Marshal(routes)
 	return &grpcproto.JsonResponse{JsonResponse: byteData}, err
